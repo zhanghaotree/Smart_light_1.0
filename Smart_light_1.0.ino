7f39bfb43406ccae08d1a1a0fd90cbd6d1d3e0b7
zhanghaotree <909799293@qq.com>
2018-08-16 14:39:37 +0800

    测试

--- Smart_light_1.0.ino
+++ Smart_light_1.0.ino
@@ -1,4 +1,17 @@
-#include <ArduinoOTA.h>
+/*
+ * Smart_Light_V1.0
+ * Designed by ZhangHao
+ * 2018.8.16
+ * note:
+ * 1.实现MQTT连接，协议定义与解析。
+ * 2.实现OTA升级功能
+ * 3.实现白光灯和暖白灯的PWM调节
+ * 4.RGB灯驱动程序
+ * 5.光敏电阻测环境光亮度
+ * 6.声音传感器驱动程序
+ * 7.继电器驱动程序
+ * 详见ReadMe.md文件
+ */
 
 void setup() {
   // put your setup code here, to run once:
